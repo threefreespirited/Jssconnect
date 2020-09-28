5ab685165854719d5fa58e813fc8487a7071553c
Harshit Sharma <47037260+me-harshit@users.noreply.github.com>
2020-09-28 19:48:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -988,13 +988,13 @@ app.get("/community", (req, res) => {
             if (commData[j].email == data[i].email) {
               myImage = commData[j].picture;
             }
-	    else{
-	      myImage = "https://cdn5.vectorstock.com/i/thumb-large/54/69/male-user-icon-vector-8865469.jpg";
-	    }
           }
 		var myGithub = data[i].gHub;
 		var myLinkedIn = data[i].lIn;
-		
+	
+        if(myImage == ""){
+           myImage = "https://cdn5.vectorstock.com/i/thumb-large/54/69/male-user-icon-vector-8865469.jpg";
+	}
 	if(myGithub == ""){
 	   myGithub = "https://github.com/";
 	}
