2fcba4ffd4f2c28f092d9a22024e945ccf74d96b
Harshit Sharma <47037260+harshit9270@users.noreply.github.com>
2021-04-20 13:18:59 +0530

    update callback url

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ passport.use(
   new GoogleStrategy({
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/home",
+      callbackURL: "/auth/google/home",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
