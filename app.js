3e2b40854e8c63161023fd524678c42176e46139
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-11-22 13:23:49 +0530

    Updated redirect uri

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/home",
+      callbackURL: "https://jssconnect.herokuapp.com/auth/google/home",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
